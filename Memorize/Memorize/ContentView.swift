cdc85565e13e63a2f0ca5d48f47616f388c9d609
XCWell <1901948779@qq.com>
2021-12-07 14:04:51 +0800

    L2.1 临时方案  添加底部按钮  数组emojis

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,33 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🐶","🐱","🐭","🐹", "🐰","🦊","🐻","🐼","🐻‍❄️","🐨","🐯","🦁","🐮","🐷","🐽","🐸","🐵","🙈","🙉"," 🙊","🐒","🐔","🐧","🐦","🐤"," 🐣","🐥","🦆","🦅","🦉","🦇",]
+    @State var horCount = 4
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            HStack {
+                ForEach(emojis[0..<horCount],id: \.self, content: {emoji in
+                    CardView(content: emoji)
+                })
+            }
+            Spacer()
+            HStack {
+                removeView
+                Spacer()
+                addView
+            }.padding(.horizontal).font(.largeTitle)
+        }
+        .padding()
+        .foregroundColor(.red)
+    }
+    
+    var removeView : some View {
+        Button {
+            if horCount > 1 {
+                horCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
         }
     }
+    var addView : some View {
+        Button {
+            if horCount < emojis.count {
+                horCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+    
+    
 }
 
 struct CardView : View {
     
     @State var isFaceUp = true
-    
+    var content: String
     var body: some View {
         let shape = RoundedRectangle(cornerRadius: 20)
         ZStack {
             if isFaceUp {
                 shape.stroke(lineWidth: 3)
                 shape.fill().foregroundColor(.white)
-                Text("🐶").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
         }
-        .padding()
-        .foregroundColor(.red)
         
         .onTapGesture {
             isFaceUp = !isFaceUp
@@ -46,7 +76,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
+                .previewLayout(.device)
+                .previewDevice("iPhone 13 mini")
             ContentView()
+                .previewLayout(.device)
+                .previewDevice("iPhone 13 mini")
                 .preferredColorScheme(.dark)
         }
     }
