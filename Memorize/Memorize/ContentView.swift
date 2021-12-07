a5f5c53bde015079705d1e1c08795cf5e8a60256
XCWell <1901948779@qq.com>
2021-12-07 11:03:35 +0800

    L2.0  自定义CardView

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,17 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+    }
+}
+
+struct CardView : View {
+    
+    @State var isFaceUp = true
+    
+    var body: some View {
+        let shape = RoundedRectangle(cornerRadius: 20)
         ZStack {
-            RoundedRectangle(cornerRadius: 20).stroke(lineWidth: 3)
-            Text("Hello, world!")
+            if isFaceUp {
+                shape.stroke(lineWidth: 3)
+                shape.fill().foregroundColor(.white)
+                Text("🐶").font(.largeTitle)
+            } else {
+                shape.fill()
+            }
         }
         .padding()
         .foregroundColor(.red)
+        
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
